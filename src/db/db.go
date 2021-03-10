61c2032e40b0fc171f1b1a88e6d82a93681984c4
BananaFried <35321424+BananaFried525@users.noreply.github.com>
2021-03-10 15:56:50 +0700

    Update db.go

--- src/db/db.go
+++ src/db/db.go
@@ -9,10 +9,10 @@ import (
 )
 
 func ConnectDB() *sql.DB {
-	dbURL := "ec2-18-211-97-89.compute-1.amazonaws.com:5432"
-	dbName := "de3dgdv7gidb12"
-	dbUser := "krtelnhvanlslj"
-	dbPsswrd := "f300b8d256d3bb11c895c01d9e46d2881d2c63a5f0ebf2813d7672ad4ea46f7c"
+	dbURL := "abc"
+	dbName := "abc"
+	dbUser := "abc"
+	dbPsswrd := "abc"
 
 	connectStr := fmt.Sprintf(`postgres://%v:%v@%v/%v`, dbUser, dbPsswrd, dbURL, dbName)
 	conn, err := sql.Open("postgres", connectStr)
